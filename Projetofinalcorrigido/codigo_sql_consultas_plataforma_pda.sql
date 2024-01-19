deffa68dcc503be2fdcb3494f210df997883ebd9
Ingrid <oligrid20@gmail.com>
2024-01-19 09:22:32 -0300

    novas consultas

--- Projetofinalcorrigido/codigo_sql_consultas_plataforma_pda.sql
+++ Projetofinalcorrigido/codigo_sql_consultas_plataforma_pda.sql
@@ -1,21 +1,21 @@
 use plataforma_pda;
-select * from pda_platform.tb_student;
-/*união de chave id_user e estudante e turma */
+
+--união de chave id_user e estudante e turma 
 select u.*, c.id_class, c.class_name, s.student_registration, s.id_user
 from
     tb_user u
     inner join tb_student s on u.id_user = s.id_user
     inner join tb_class c on s.id_class = c.id_class;
 
-/*média das idades dos alunos*/
+--média das idades dos 23 alunos 
 use pda_platform;
-select
+ select
     avg((round(datediff(curdate(), u.user_birth_date) / 365.25))) as media_idade_studants
-from
+ from
     tb_user u
-    inner join tb_student s on s.id_user = u.id_user;
+ inner join tb_student s on s.id_user = u.id_user;
 
- /*idade dos alunos*/
+ --idade dos alunos
 use pda_platform;
 select
     u.id_user,
@@ -48,6 +48,7 @@ from
 where
 /* aqui escolhe a turma*/
 c.class_name = 'turma 2';
+-- consulta para puxar quantos alunos ficaram com nota escolhida na a.grade
 select
 count(*) as total_alunos_nota
 from
@@ -55,5 +56,17 @@ from
     inner join tb_student s on tb_user.id_user = s.id_user
     inner join  tb_activity a on s.id_user = a.id_student
 where
-/* aqui escolhe a nota*/
+/* aqui escolhe a nota ex: a.grade >= 7 and (a.grade < 10) */
 a.grade <= 6;
+
+-- consulta para puxar o nome dos alunos que tiraram nota que escolher na a.grade
+select
+u.user_name,
+a.grade
+from
+   tb_user u
+    inner join tb_student s on u.id_user = s.id_user
+    inner join  tb_activity a on s.id_user = a.id_student
+where
+/* aqui escolhe a nota*/
+a.grade >= 7 and (a.grade < 10);
